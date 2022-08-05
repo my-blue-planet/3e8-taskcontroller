b89ca59ed745fa1893bf354198b36ececbbe2c86
my-blue-planet <adrian.luethi+myplanet@gymburgdorf.ch>
2022-08-05 09:17:28 +0200

    infrastructure

--- myPostbuilder.js
+++ myPostbuilder.js
@@ -1,5 +1,3 @@
-// https://www.html5rocks.com/en/tutorials/workers/basics/#toc-inlineworkers
-//
 import fs from "fs"
 
 
@@ -7,4 +5,6 @@ import fs from "fs"
 // fs.writeFileSync("./toNPM/runPython.js", maincodeOut, {encoding: "utf8"})
 fs.copyFileSync("./dist/taskcontroller.js", "./toNPM/taskcontroller.js")
 fs.copyFileSync("./src/TaskController.d.ts", "./toNPM/taskcontroller.d.ts")
+fs.copyFileSync("./src/TaskMenu.d.ts", "./toNPM/TaskMenu.d.ts")
+fs.copyFileSync("./src/VersionManager.d.ts", "./toNPM/VersionManager.d.ts")
 fs.copyFileSync("./dist/style.css", "./toNPM/style.css")
